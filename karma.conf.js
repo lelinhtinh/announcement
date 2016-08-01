49c77f3df3d334aa71ebb07c237ead2f9866fc7f
Zzbaivong <baivong@users.noreply.github.com>
2016-08-02 00:04:10 +0700

    format

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,32 @@
 module.exports = function(config) {
-  config.set({
-    frameworks: ['qunit'],
-    files: [
-      'node_modules/jquery/dist/jquery.min.js',
-      'src/*.js',
-      'test/setup.js',
-      'test/spec/*.js',
-      'demo/*.js'
-    ],
-    preprocessors: {
-      'src/*.js': ['eslint'],
-      'test/**/*.js': ['eslint'],
-      'demo/*.js': ['eslint']
-    },
-    eslint: {
-      engine: {
-        configFile: '.eslintrc.json'
-      },
-      showWarnings: true,
-      stopOnError: false
-    },
-    reporters: ['progress'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['PhantomJS', 'Firefox'],
-    singleRun: false,
-    concurrency: Infinity
-  });
+    config.set({
+        frameworks: ['qunit'],
+        files: [
+            'node_modules/jquery/dist/jquery.min.js',
+            'src/*.js',
+            'test/setup.js',
+            'test/spec/*.js',
+            'demo/*.js'
+        ],
+        preprocessors: {
+            'src/*.js': ['eslint'],
+            'test/**/*.js': ['eslint'],
+            'demo/*.js': ['eslint']
+        },
+        eslint: {
+            engine: {
+                configFile: '.eslintrc.json'
+            },
+            showWarnings: true,
+            stopOnError: false
+        },
+        reporters: ['progress'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+        browsers: ['PhantomJS', 'Firefox'],
+        singleRun: false,
+        concurrency: Infinity
+    });
 };
